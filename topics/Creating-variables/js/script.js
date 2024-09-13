df91ecf103be33876024d8d1ff50963e85038deb
TerenceThw <terencexdd@gmail.com>
2024-09-12 21:23:40 -0400

    draw a peice of cheese

--- topics/Creating-variables/js/script.js
+++ topics/Creating-variables/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * creating variables
+ * Pippin Barr
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -12,13 +12,23 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(480,480);
 }
 
 
 /**
+ * *draw a hole in a piece of cheese
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+
+    background(255,250,0);
+
+    //the hole
+    push();
+    noStroke();
+    fill(0);
+    ellipse(140,175,180);
+    pop();
 
 }
