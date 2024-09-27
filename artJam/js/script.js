e7fd9b3e011f31a365397b71bc791ee136e10128
TerenceThw <terencexdd@gmail.com>
2024-09-26 20:45:04 -0400

    created the background and the virus

--- artJam/js/script.js
+++ artJam/js/script.js
@@ -1,18 +1,38 @@
 /**
- * Title of Project
- * Author Name
+ * the Virus
+ * Terence Tamg
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * you, as a scientist, try to save the world from 
+ * letting the virus explode. You can either do nothing 
+ * or try to stop the virus from spreading among the public.
+ * 
+ * contorls:
+ * - use the mouse to stop the virus (or not)
+ * 
+ * uses:
+ * p5.ps
+ * https://p5js.org
  */
 
 "use strict";
+
+// create the arraly of virus
+// There will be one virus when the game starts
+let vrius =[
+    {
+        x: 300,
+        y: 300,
+        size: 100
+    },
+]
+
+
 
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(600,600);
 }
 
 
@@ -20,5 +40,5 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-
+    background("#000000");
 }
